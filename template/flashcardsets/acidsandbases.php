bd8518e40dc151a0c2462e18647ba125aef391ef
Nozz <nozolo90@gmail.com>
2020-12-05 19:24:19 +0000

    demo flashcardset.php

---
+++ template/flashcardsets/acidsandbases.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+	<title>Flashcard set for acids and bases</title>
+	<link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+	<header class="box">
+		<h1>Flashcard set for acids and bases</h1>
+	</header>
+	
+	<section class="box">
+		<table>
+			<tr>
+				<td>The definition of an acid in arrhenious' theory</td>
+				<td>A substance that dissociates in water to produce hydrogen ions</td>
+			</tr>
+		</table>
+	</section>
+	
+	<footer class="box">
+		<center><a href="../index.html">Home</a></center>
+	</footer>
+</body>
