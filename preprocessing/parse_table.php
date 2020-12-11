b6c1812a298b5030281f93fbb00050868dcada54
Nozz <nozolo90@gmail.com>
2020-12-11 01:31:45 +0000

    added hurlamboc stuff

--- preprocessing/parse_table.php
+++
@@ -1,5 +0,0 @@
-<?php
-	function parse_table($filepath) {
-		
-	}
-?>
--- template/notes/irish/hurlamboc.php
+++ template/notes/irish/hurlamboc.php
@@ -1,15 +1,24 @@
+<?php require("preprocessing/utilities.php")?>
+
 <!DOCTYPE html>
 
 <head>
 	<title>Hurlamboc</title>
 	<link rel="stylesheet" href="../../style.css">
+	<meta charset="utf-8">
 </head>
 <body>
 	<header><h1>Hurlamboc</h1></header>
 	<section>
 	
+	<h2>Vocabulary</h1>
+	<?php print(file_to_table("template/notes/irish/hurlamboc.txt"))?>
+
+	<h2>Téama an úrscéil</h2>
+	<p>Is é saol le linn ré an tíogair cheiltigh agus an t-ábharachas a bhí ag baint leis téama an úrscéal seo. Tugann Éilis Ní Duibhne léargas dúinn faoi na laethanta na meala fadó fadó. Bhí daoine saibhir, ach bhí siad bochta ó thaobh spiorad agus cultúr<p>
 	
-	
+	<p>I dtús báire, an t-ábharachas. Ar an dromchlá, ceapfá go mbeadh clann foirfe é clann Allbright. Tá sé rathúil agus san ardaicme. Tuilleann Pól Allbright go leor airgid, agus níl ar Lisín dul amach ag obair. Chomh maith le sin, tá arasáin aige ar fud na hEoropa. Áfach, sin é an chuid is lú den scéal. Faoin saibhreas agus rachmais, tá na rudaí tabhachtach an tsaoil imithe. Mar shampla, tá caidreamh lag ag Lisín lena mac féin. Cuireann sí lagmisneach air, ach níl Ruán in ann dul i ngléic léi. Ceapann sé go gcaitheann sé rá pé ab mhaith a mháthair. De bharr seo, tuigimid go raibh daoine gafa le hairgead le linn ré an tíogair cheiltigh, ach rinne siad faillí ar a gcultúr agus a gclainne.</p>
+
 	</section>
 	<footer><a href="../../index.html">Home</a></footer>
 </body>
