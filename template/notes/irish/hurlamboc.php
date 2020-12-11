46914222626b67625cef9a2e6f16c4b685fc3606
Nozz <nozolo90@gmail.com>
2020-12-11 16:41:07 +0000

    Hurlamboc added

--- template/notes/irish/hurlamboc.php
+++ template/notes/irish/hurlamboc.php
@@ -19,6 +19,8 @@
 	
 	<p>I dtús báire, an t-ábharachas. Ar an dromchlá, ceapfá go mbeadh clann foirfe é clann Allbright. Tá sé rathúil agus san ardaicme. Tuilleann Pól Allbright go leor airgid, agus níl ar Lisín dul amach ag obair. Chomh maith le sin, tá arasáin aige ar fud na hEoropa. Áfach, sin é an chuid is lú den scéal. Faoin saibhreas agus rachmais, tá na rudaí tabhachtach an tsaoil imithe. Mar shampla, tá caidreamh lag ag Lisín lena mac féin. Cuireann sí lagmisneach air, ach níl Ruán in ann dul i ngléic léi. Ceapann sé go gcaitheann sé rá pé ab mhaith a mháthair. De bharr seo, tuigimid go raibh daoine gafa le hairgead le linn ré an tíogair cheiltigh, ach rinne siad faillí ar a gcultúr agus a gclainne.</p>
 
+	<h2>Aoir glic</h2>
+	<p>Tá aoir go mór chun cinn sa sliocht seo</p>
 	</section>
 	<footer><a href="../../index.html">Home</a></footer>
 </body>
