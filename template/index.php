65bd50308756540d128e856dfd9eb2298cc69ee0
Naza <uzoukwuc@tcd.ie>
2023-01-27 20:10:18 +0000

    Added index for new homepage

---
+++ template/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<head>
+	<title>Naza's Website</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+
+<body>
+	<h1>Naza's website <small>(<a href="https://nassir90.github.io/nu-output/">secure version</a>)</small></h1>
+	
+	<section>
+                <p>Under construction ...</p>
+
+                <p><a href="index-before-2022.html">Content from before 2022</a></p>
+	</section>
+
+	<footer>
+		<h2>General links</h2>
+		<ul>
+			<li><a href="https://openra.net">OpenRA - an RTS game</a></li>
+			<li><a href="https://github.com/nassir90/nu">Github repo</a></li>
+			<li><a href="https://z-lib.org">Free books</a>
+			<li><a href="https://wttr.in">Instant weather</a></li>
+			<li><a href="https://iteroni.com">Youtube client without the crud</a></li>
+		</ul>
+	</footer>
+</body>
